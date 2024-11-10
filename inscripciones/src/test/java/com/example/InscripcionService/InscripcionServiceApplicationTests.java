fc33431fc4d6640c19394ac2fd617b05526ba123
Ignacio <ignaher12@gmail.com>
2024-11-10 15:04:07 -0300

    init proyect

---
+++ inscripciones/src/test/java/com/example/InscripcionService/InscripcionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InscripcionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscripcionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
